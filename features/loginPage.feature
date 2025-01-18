c292dc92a448eb3c5746290db4ac7edb0cb04aa9
noyonsarder <noyonsarder87@gmail.com>
2025-01-19 00:38:47 +0600

    QA-2:Done my test

--- features/loginPage.feature
+++ features/loginPage.feature
@@ -1,13 +1,39 @@
-Feature: Login Feature
+Feature: Login and Logout Feature
 
-    Scenario Outline: Login form test with "<test>" scenario
+    Scenario Outline: Test the login form with "<test>" credentials
         Given I navigate to the test site
         When I enter "<username>" and "<password>"
         And I click "LOGIN" button
-        Then the text "If you do not have an account, contact an admin" should display
+        Then the following text "<message>" should display
 
         Examples:
-            | test        | username | password       |
-            | Positive    | SomeName | TopSecret1234! |
-            | emptySubmit |          |                |
-            | Negative    | @#       | $%             |
+            | test    | username      | password       | message                                         |
+            | Invalid | SomeName      | TopSecret1234! | If you do not have an account, contact an admin |
+            | Invalid | @#            |                | If you do not have an account, contact an admin |
+            | Valid   | SomeUser_name | TopSecret1234! | Hello SomeName                                  |
+
+    Scenario: Log out of the application after logging in
+        Given I navigate to the test site
+        When I enter "SomeUser_name" and "TopSecret1234!"
+        And I click "LOGIN" button
+        Then the following text "Hello SomeName" should display
+        When I click "LOGOUT" button
+        Then the following text "qa.code-quiz.dev" should display
+
+    Scenario: Verify the user information after logging in
+        Given I navigate to the test site
+        When I enter "SomeUser_name" and "TopSecret1234!"
+        And I click "LOGIN" button
+        Then the following user information should display
+            | name            | SomeName   |
+            | favouriteFruit  | some fruit |
+            | favouriteMovie  | The Room   |
+            | favouriteNumber | BN<1234>   |
+
+    Scenario: The session expires unexpectedly when the page is reloaded after logging in
+        Given I navigate to the test site
+        When I enter "SomeUser_name" and "TopSecret1234!"
+        And I click "LOGIN" button
+        Then the following text "Hello SomeName" should display
+        When I reload the page
+        Then the following text "qa.code-quiz.dev" should display
--- step_definitions/loginStep.js
+++ step_definitions/loginStep.js
@@ -1,22 +1,45 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
-const {expect} = require("@playwright/test");
+const { expect } = require("@playwright/test");
 
 Given('I navigate to the test site', async function () {
     await this.page.goto('http://localhost:8080');
 })
 
-When('I enter {string} and {string}', async function (username,password) {
-    await this.page.pause();
+When('I enter {string} and {string}', async function (username, password) {
     await this.page.locator(`//input[contains(@placeholder,'Username')]`).fill(username);
     await this.page.locator(`//input[contains(@placeholder,'password')]`).fill(password);
-})
-
-When('I click {string} button', async function (loginBtn) {
     await this.page.waitForSelector(`//button[text()='LOGIN']`);
-    await this.page.locator(`//button[text()='${loginBtn}']`).click();
 })
 
-Then('the text {string} should display', async function (text) {
-    const elementText= await this.page.locator(`//div[contains(text(), 'contact an admin')]`).textContent();
-    expect(elementText).toContain(text); 
+When('I click {string} button', async function (btn) {
+    await this.page.locator(`//button[text()='${btn}']`).click();
+})
+
+Then('the following text {string} should display', async function (text) {
+    const elementText = await this.page.locator(`//div[contains(text(), '${text}')]`).textContent();
+    expect(elementText).toContain(text);
+})
+
+Then('the following user information should display', async function (table) {
+    const element = table.rowsHash();
+    if (element.name) {
+        const elementLocator = await this.page.locator(`//div[text()='${element.name}']`).textContent();
+        expect(elementLocator).toContain(element.name);
+    }
+    if (element.favouriteFruit) {
+        const elementLocator = await this.page.locator(`//div[text()='${element.favouriteFruit}']`).textContent();
+        expect(elementLocator).toContain(element.favouriteFruit);
+    }
+    if (element.favouriteMovie) {
+        const elementLocator = await this.page.locator(`//div[text()='${element.favouriteMovie}']`).textContent();
+        expect(elementLocator).toContain(element.favouriteMovie);
+    }
+    if (element.favouriteNumber) {
+        const elementLocator = await this.page.locator(`//div[text()='${element.favouriteNumber}']`).textContent();
+        expect(elementLocator).toContain(element.favouriteNumber);
+    }
+})
+
+When('I reload the page', async function () {
+    await this.page.reload();
 })
